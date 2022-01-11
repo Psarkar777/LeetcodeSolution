3c13f2fb088e54c8c8fde1ad4c8e31628947602e
Psarkar777 <56498690+Psarkar777@users.noreply.github.com>
2022-01-11 11:44:07 +0530

    Add files via upload

---
+++ BuyAndSell_2.java
@@ -0,0 +1,57 @@
+// Leetcode : 122
+/*
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Total profit is 4.
+
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+ */
+
+
+package Leetcode;
+
+import java.util.Scanner;
+
+public class BuyAndSell_2 {
+    public static int maxProfit(int[] prices) {
+        int profit = 0;  // Initialize
+        for(int i=1;i<prices.length;i++) {
+            // If price on the day greater than the previous day
+            if(prices[i] > prices[i-1]) {
+                profit += (prices[i]-prices[i-1]);
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(maxProfit(arr));
+    }
+}
